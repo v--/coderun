70d005422a025e56c2c78f914c8eb8a1830804f4
toshle <rosen_ra4ev@abv.bg>
2014-11-08 13:11:51 +0200

    Add Player logger

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -1,5 +1,6 @@
 function Player(game) {
 
+  this.logger = Logger.get('player');
   this.game = game;
   this.sprite = null;
   this.controls = null;
@@ -9,12 +10,12 @@ function Player(game) {
 Player.prototype = {
 
   preload: function() {
-    logger.info("Loading player sprite.");
+    this.logger.info("Loading player sprite.");
     this.game.load.spritesheet('player', 'img/man.png', 32, 48);
   },
 
   create: function() {
-    logger.info("Creating player.");
+    this.logger.info("Creating player.");
     this.sprite = game.add.sprite(32, 100, 'player');
   },
 
