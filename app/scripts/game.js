04d5ea1ba90ca7ab041ee4aed51313204c347d03
toshle <rosen_ra4ev@abv.bg>
2014-11-08 12:12:12 +0200

    Create Player class Add preload, create and update functions to game.js

--- app/scripts/game.js
+++ app/scripts/game.js
@@ -1,5 +1,23 @@
 var phaserContainer = document.getElementById('phaser');
-var game = new Phaser.Game(phaserContainer.scrollWidth, phaserContainer.scrollHeight, Phaser.AUTO, phaserContainer, {}, true);
+var game = new Phaser.Game(phaserContainer.scrollWidth, phaserContainer.scrollHeight, Phaser.AUTO, phaserContainer, { preload: preload, create: create, update: update }, true);
+
+var player = null;
+var level = null;
+
+function preload() {
+  
+  player = new Player(game);
+  player.preload();
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
 
 window.addEventListener('resize', function () {
     game.renderer.resize(phaserContainer.scrollWidth, phaserContainer.scrollHeight);
---
+++ app/scripts/player.js
@@ -0,0 +1,23 @@
+function Player(game) {
+
+  this.game = game;
+  this.sprite = null;
+  this.controls = null;
+
+}
+
+Player.prototype = {
+
+  preload: function() {
+    //this.game.load.spritesheet('player', 'images/player.png', 32, 48);
+  },
+
+  create: function() {
+
+  },
+
+  update: function() {
+
+  }
+
+}
